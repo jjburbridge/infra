1c2df451c6436729b37657c12c369f20d424f6e3
Jon Burbridge <jonathan.burbridge@sanity.io>
2026-08-11 10:36:54 +0100

    missing comma

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,9 +1,9 @@
-import {type SanityConfig} from '@sanity/sdk'
-import {SanityApp} from '@sanity/sdk-react'
-import {Flex, Spinner} from '@sanity/ui'
-import {dataset, projectId} from './env'
-import {PostList} from './PostList'
-import {SanityUI} from './SanityUI'
+import { type SanityConfig } from "@sanity/sdk";
+import { SanityApp } from "@sanity/sdk-react";
+import { Flex, Spinner } from "@sanity/ui";
+import { dataset, projectId } from "./env";
+import { PostList } from "./PostList";
+import { SanityUI } from "./SanityUI";
 
 // apps can access many different projects or other sources of data
 const sanityConfigs: SanityConfig[] = [
@@ -11,14 +11,19 @@ const sanityConfigs: SanityConfig[] = [
     projectId,
     dataset,
   },
-]
+];
 
 function Loading() {
   return (
-    <Flex justify="center" align="center" height="fill" style={{width: '100vw' color: 'red'}}>
+    <Flex
+      justify="center"
+      align="center"
+      height="fill"
+      style={{ width: "100vw", color: "red" }}
+    >
       <Spinner />
     </Flex>
-  )
+  );
 }
 
 function App() {
@@ -28,7 +33,7 @@ function App() {
         <PostList />
       </SanityApp>
     </SanityUI>
-  )
+  );
 }
 
-export default App
+export default App;
